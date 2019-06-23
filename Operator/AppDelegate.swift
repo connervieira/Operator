fddf72b1ff76dd717500b80fed56d0071e8d8c73
Conner Vieira <vieirast@icloud.com>
2019-06-23 15:22:29 -0400

    Initial Commit

---
+++ Operator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Operator
+//
+//  Created by Conner Vieira on 6/23/19.
+//  Copyright © 2019 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Operator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Operator
+//
+//  Created by Conner Vieira on 6/23/19.
+//  Copyright © 2019 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
